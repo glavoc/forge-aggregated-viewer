8f8a5147e62fff03e225a79ce74dbef409ed77ff
glavoc <lumoric@gmail.com>
2021-04-08 22:25:21 +1200

    staging

---
+++ wwwroot/js/ForgeTree.js
@@ -0,0 +1,185 @@
+//this document gets and sets Forge token, clientid, 
+//and handles sign in button
+
+
+//this function is JQuery and will run only when DOM (Document Object Model)
+//is ready for JS code to execute
+//here we'll route client to oauthentication url
+$(document).ready(function () {
+  $.ajax({
+    url: '/api/forge/oauth/token',
+    success: function (res) {
+      //got token, means user is signed in
+      $('#signIn').hide();
+      $('#signOut').show();
+      // finally:
+      prepareUserHubsTree();
+      console.log($('#userHubs').jstree())
+    }
+  })
+
+  $('#signIn').click(function () {
+    //Perform an asynchronous HTTP (Ajax) request
+    $.ajax({
+      url: '/api/forge/oauth/url',
+      //i thought that it'll redirect to url on success, but i'm wrong
+      //!!!  further investigation needed
+      success: function (url) {
+        location.href = url;
+      }
+    })
+  })
+
+  $('#signOut').click(function () {
+    location.href = '/api/forge/oauth/signout';
+    $('#signOut').hide();
+    $('#signIn').show();
+  })
+
+
+});
+
+function prepareUserHubsTree() {
+  $('#userHubs').jstree({
+    'core': {
+      'themes': { "icons": true },
+      'multiple': false,
+      'data': {
+        "url": '/api/forge/datamanagement',
+        "dataType": "json",
+        'cache': false,
+        'data': function (node) {
+          $('#userHubs').jstree(true).toggle_node(node);
+          return { "id": node.id };
+        }
+      }
+    },
+    'types': {
+      'default': { 'icon': 'glyphicon glyphicon-question-sign' },
+      '#': { 'icon': 'glyphicon glyphicon-user' },
+      'hubs': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/a360hub.png' },
+      'personalHub': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/a360hub.png' },
+      'bim360Hubs': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/bim360hub.png' },
+      'bim360projects': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/bim360project.png' },
+      'a360projects': { 'icon': 'https://github.com/Autodesk-Forge/bim360appstore-data.management-nodejs-transfer.storage/raw/master/www/img/a360project.png' },
+      'folders': { 'icon': 'glyphicon glyphicon-folder-open' },
+      'items': { 'icon': 'glyphicon glyphicon-file' },
+      'bim360documents': { 'icon': 'glyphicon glyphicon-file' },
+      'versions': { 'icon': 'glyphicon glyphicon-time' },
+      'unsupported': { 'icon': 'glyphicon glyphicon-ban-circle' }
+    },
+    "sort": function (a, b) {
+      var a1 = this.get_node(a);
+      var b1 = this.get_node(b);
+      var parent = this.get_node(a1.parent);
+      if (parent.type === 'items') { // sort by version number
+        var id1 = Number.parseInt(a1.text.substring(a1.text.indexOf('v') + 1, a1.text.indexOf(':')))
+        var id2 = Number.parseInt(b1.text.substring(b1.text.indexOf('v') + 1, b1.text.indexOf(':')));
+        return id1 > id2 ? 1 : -1;
+      }
+      else if (a1.type !== b1.type) return a1.icon < b1.icon ? 1 : -1; // types are different inside folder, so sort by icon (files/folders)
+      else return a1.text > b1.text ? 1 : -1; // basic name/text sort
+    },
+    "plugins": ["types", "state", "sort"],
+    "state": { "key": "autodeskHubs" }// key restore tree state
+  }).bind("activate_node.jstree", function (evt, data) {
+    if (data != null && data.node != null && (data.node.type == 'versions' || data.node.type == 'bim360documents')) {
+      // in case the node.id contains a | then split into URN & viewableId
+      if (data.node.id.indexOf('|') > -1) {
+        var urn = data.node.id.split('|')[1];
+        var viewableId = data.node.id.split('|')[2];
+        console.log(urn);
+        launchViewer(urn, viewableId);
+      }
+      else {
+        console.log(data.node.id);
+        //launchViewer(data.node.id);
+        const models = [
+          { urn: 'urn:dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLjNHMkxBZTVYUkdhOUJ4SWZ0aElXNnc_dmVyc2lvbj0z' },
+          { urn: 'urn:dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLjNHMkxBZTVYUkdhOUJ4SWZ0aElXNnc_dmVyc2lvbj0y' },
+          { urn: 'urn:dXJuOmFkc2sud2lwcHJvZDpmcy5maWxlOnZmLjNHMkxBZTVYUkdhOUJ4SWZ0aElXNnc_dmVyc2lvbj0x' },
+        ];
+        launchViewer(models.concat());
+      }
+    }
+  });
+}
+
+function getForgeToken(callback) {
+  fetch('/api/forge/oauth/token').then(res => {
+    res.json().then(data => {
+      callback(data.access_token, data.expires_in);
+    });
+  });
+  
+}
+
+function launchViewer( models ) {
+  if( !models || models.length <= 0 )
+    return console.error( 'Empty model input' );
+
+  const options = {
+    env: 'AutodeskProduction',
+    getAccessToken: getForgeToken
+  };
+
+  const options3d = {
+    viewerConfig: {
+      disableBimWalkInfoIcon: true
+    }
+  };
+
+  function loadManifest( documentId ) {
+    return new Promise(( resolve, reject ) => {
+      const onDocumentLoadSuccess = ( doc ) => {
+        doc.downloadAecModelData(() => resolve(doc));
+      };
+      Autodesk.Viewing.Document.load( documentId, onDocumentLoadSuccess, reject );
+    });
+  }
+
+  Autodesk.Viewing.Initializer( options, function() {
+    //get the viewer div
+    const viewerDiv = document.getElementById( 'forgeViewer' );
+
+    //initialize the viewer object
+    const view = new Autodesk.Viewing.AggregatedView();
+    view.init( viewerDiv, options3d );
+
+    const viewer = view.viewer;
+
+    const tasks = [];
+    models.forEach( md => tasks.push( loadManifest( md.urn ) ) );
+
+
+    Promise.all(tasks)
+            .then( docs =>  Promise.resolve( docs.map( doc => {
+              const bubbles = doc.getRoot().search({type:'geometry', role: '3d'});
+              const bubble = bubbles[0];
+              if( !bubble ) return null;
+
+              return bubble;
+            })))
+            .then( bubbles => view.setNodes( bubbles ) );
+  });
+}
+
+
+//toggle sidebar visibility
+function openNav() {
+  var navbarVisible = document.getElementById("userHubs").style.width;
+  if (navbarVisible == "0px") {
+    document.getElementById("projectNavigate").style.width = "400px";
+    document.getElementById("userHubs").style.width = "360px";
+    document.getElementById("userHubs").style.transition = "width 1s ease-in-out";
+  }
+  else {
+    document.getElementById("projectNavigate").style.width = "40px";
+    document.getElementById("userHubs").style.width = "0px";
+    document.getElementById("userHubs").style.transition = "width 1s ease-in-out";
+  }
+}
+
+
+
+
---
+++ wwwroot/js/extensions/Dashboard/Dashboard.js
@@ -0,0 +1,69 @@
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+window.dashboard = null
+
+$(document).ready(function () {
+    $(document).on('DOMNodeInserted', function (e) {
+        if ($(e.target).hasClass('orbit-gizmo')) {
+            // to make sure we get the viewer, let's use the global var NOP_VIEWER
+            if (NOP_VIEWER === null || NOP_VIEWER === undefined) return;
+            dashboard = new Dashboard(NOP_VIEWER, [
+                new BarChart('Material'),
+                new PieChart('Material')
+            ])
+        }
+    });
+})
+
+// Handles the Dashboard panels
+class Dashboard {
+    constructor(viewer, panels) {
+        var _this = this;
+        this._viewer = viewer;
+        this._panels = panels;
+
+        this._viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, (viewer) => {
+            this.adjustLayout();
+            setTimeout(function () { _this._viewer.resize() }, 500); _this._viewer.resize();
+            _this.loadPanels();
+        });
+    }
+
+    adjustLayout() {
+        console.log("Dashboard loaded");
+        // this function may vary for layout to layout...
+        // for learn forge tutorials, let's get the ROW and adjust the size of the 
+        // columns so it can fit the new dashboard column, also we added a smooth transition css class for a better user experience
+        var viewerContainer = $("#viewerContainer");
+        // TODO: check if dashboard is loaded and kill it
+        viewerContainer.after('<div id="dashboard">Reports</div>');
+    }
+
+    loadPanels() {
+        var _this = this;
+        var data = new ModelData(this);
+        data.init(function () {
+            $('#dashboard').empty();
+            _this._panels.forEach(function (panel) {
+                // let's create a DIV with the Panel Function name and load it
+                panel.load('dashboard', viewer, data);
+            });
+        });
+    }
+}

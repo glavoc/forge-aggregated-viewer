61b40f24fd5647f195a07a3b5b6c14357af922c9
glavoc <lumoric@gmail.com>
2021-06-29 20:01:05 +1200

    asdf

--- wwwroot/js/ForgeViewer.js
+++ wwwroot/js/ForgeViewer.js
@@ -40,7 +40,7 @@ function launchViewer(urn) {
     view = new Autodesk.Viewing.AggregatedView();
     view.init(viewerDiv, options3d);
     viewer = view.viewer;
-    var callback = function () { };
+    var callback = function () {};
     Autodesk.Viewing.Initializer(options, callback);
   }
   //call dashboard loader once geometry has loaded
@@ -104,8 +104,15 @@ function populateDashboard() {
 
   data.init(function () {
     var chartData = [];
-    var width = 450
-    var height = 450
+    for (material in data.getLabels(propertyName)) {
+      var input = {};
+      input['Name'] = material;
+      input['Values'] = data.getIds(propertyName, material);
+      input['Size'] = data.getIds(propertyName, material).length;
+      chartData.push(input)
+    }
+    var width = 400
+    var height = 400
     var margin = 50
     var radius = Math.min(width, height) / 2 - margin
 
@@ -118,33 +125,30 @@ function populateDashboard() {
       '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
       '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
       '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
+    ];
 
 
-    for (material in data.getLabels(propertyName)) {
-      var input = {};
-      input['Name'] = material;
-      input['Values'] = data.getIds(propertyName, material);
-      input['Size'] = data.getIds(propertyName, material).length;
-      chartData.push(input)
-    }
+    var svg = d3.select("#dasboardViewer")
+      .append("div")
+      // Container class to make it responsive.
+      .classed("svg-container", true)
+      .append("svg")
+      // Responsive SVG needs these 2 attributes and no width and height attr.
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 " + width + " " + height)
+      // Class to make it responsive.
+      .classed("svg-content-responsive", true)
+    //.attr("width", width)
+    //.attr("height", height)
+    //.style("display", "block")
+    //.style("margin", "auto")
 
-    var svg = d3.select("#dashboardViewer")
-      .append('svg')
-      .attr("width", width)
-      .attr("height", height)
 
     var g = svg
       .append("g")
       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
 
-
-    console.log(data.getLabels(propertyName))
-
-    svg.style("width", "100%");
-    svg.style("height", "100%");
-    svg.style("z-index", 9);
-    g.style("margin", "auto")
 
     var color = d3.scaleOrdinal()
       .domain(chartData)
@@ -152,6 +156,7 @@ function populateDashboard() {
 
     // Generate the pie
     var pie = d3.pie()
+      .sort(null)
       .value(function (d) {
         return d.Size
       })
@@ -159,63 +164,76 @@ function populateDashboard() {
     // Generate the arcs
     var arc = d3.arc()
       .innerRadius(0)
-      .outerRadius(radius-50);
+      .outerRadius(radius - 50);
     // Another arc that won't be drawn. Just for labels positioning
     var outerArc = d3.arc()
-      .innerRadius(radius*0.8)
-      .outerRadius(radius*0.8)
+      .innerRadius(radius * 0.8)
+      .outerRadius(radius * 0.6)
 
     //Generate groups
     var arcs = g.selectAll("arc")
       .data(pie(chartData))
-      .enter()
-
+      .enter().append("path")
+      .attr("fill", function (d, i) {
+        return (color(i))
+      })
+      .attr("d", arc)
+      .attr('class', "arc")
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut)
+    //.on("click", function (e, d) { alert(d.data.capacity); })
 
     //Draw arc paths
-    arcs.append("path")
-      .attr("fill", function (d, i) { return (color(i)) })
-      .attr("d", arc);
-    
-    arcs.append("text")
-      .attr("transform", function (d) {
-        return "translate(" + outerArc.centroid(d) + ")";
+    function handleMouseOver(d, i) { // Add interactivity
+      // Specify where to put label of text
+      d3.select(d.target).transition()
+        .attr("transform", "scale(1.4,1.4)")
+    }
+
+    function handleMouseOut(d, i) {
+      d3.select(d.target).transition()
+        .attr("transform", "scale(1,1)")
+    }
+
+    // Add the polylines between chart and labels:
+    g
+      .selectAll('allPolylines')
+      .data(pie(chartData))
+      .enter()
+      .append('polyline')
+      .attr("stroke", "black")
+      .style("fill", "none")
+      .attr("stroke-width", 1)
+      .attr('points', function (d) {
+        var posA = arc.centroid(d) // line insertion in the slice
+        var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+        var posC = outerArc.centroid(d); // Label position = almost the same as posB
+        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+        posC[0] = radius * 0.85 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+        return [posA, posB, posC]
       })
-      .text(function (d) { return d.Name; });
 
-// Add the polylines between chart and labels:
-arcs
-  .selectAll('allPolylines')
-  .data(pie(chartData))
-  .enter()
-  .append('polyline')
-    .attr("stroke", "black")
-    .style("fill", "none")
-    .attr("stroke-width", 1)
-    .attr('points', function(d) {
-      var posA = arc.centroid(d) // line insertion in the slice
-      var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
-      var posC = outerArc.centroid(d); // Label position = almost the same as posB
-      var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-      posC[0] = radius * 0.85 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-      return [posA, posB, posC]
-    })
 
-// Add the polylines between chart and labels:
-arcs
-  .selectAll('allLabels')
-  .data(pie(chartData))
-  .enter()
-  .append('text')
-    .text( function(d) { return d.data.Name } )
-    .attr('transform', function(d) {
+    // Add the polylines between chart and labels:
+    g
+      .selectAll('allLabels')
+      .data(pie(chartData))
+      .enter()
+      .append('text')
+      .text(function (d) {
+        console.log(d.data.Name);
+        return d.data.Names
+      })
+      .attr('transform', function (d) {
         var pos = outerArc.centroid(d);
         var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
         pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
         return 'translate(' + pos + ')';
-    })
-    .style('text-anchor', function(d) {
+      })
+      .style('text-anchor', function (d) {
         var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
         return (midangle < Math.PI ? 'start' : 'end')
-    })
-})
+      })
+
+  })
 }
